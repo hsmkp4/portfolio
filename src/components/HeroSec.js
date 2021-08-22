eea0af8011039759889972fec90ddea9202ce6c0
hes <hesam.lfl@gmail.com>
2021-08-22 13:53:21 +0430

    first edit

---
+++ src/components/HeroSec.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../css/heroSec.css';
+import logo from '../img/logo-haa';
+
+function HeroSec() {
+  return (
+    <div>
+      {/* <img src={logo} alt="" /> */}
+      <h1>Frontend Developer</h1>
+      <p>
+        Hi, I am<span>Hesam</span>,frontend developer who is a specialist on
+        <span>React</span> and JavaScript, also I have graphic design background
+        and I know a lot of CSS tricks!. I can help you to improve your business
+        with modern websites.
+      </p>
+    </div>
+  );
+}
+
+export default HeroSec;
